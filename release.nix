e7559802a37ca59476e1bdb1a776ad942eccfdf1
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-01-04 00:11:22 +0200

    Add tests

--- release.nix
+++ release.nix
@@ -3,20 +3,36 @@
 }:
     let
      pkgs = (import <nixpkgs> { system = builtins.currentSystem or "x86_64-linux"; });
-     jobs = with pkgs; {
-         laravel-tests = php.buildComposerProject (finalAttrs: {
+     jobs = with pkgs; rec {
+         laravel-build = php.buildComposerProject (finalAttrs: {
              pname = "Nix-Laravel";
              src = siteSrc;
              version = "1.0.0";
              composerNoDev = false;
              vendorHash = "sha256-cPoOZUyIE+dixrigj9l0KksJ9sduhZn6PmbRkDqzzms=";
 
-             postFixupHooks = ''
-                 cp -a .env.example .env
-                 php artisan key:generate
-                 php vendor/bin/phpunit
+             installPhase = ''
+                 runHook preInstall
+                 mkdir -p $out
+                 mv {.,}* $out
+                 runHook postInstall
              '';
+
+             passthru.tests.phpunit-tests = stdenvNoCC.mkDerivation {
+                 name ="tests";
+                 src = ./.;
+                 nativeBuildInputs = [ php ];
+                 buildPhase = ''
+                     cp -r ${laravel-build}/{.,}* .
+                 '';
+
+                 doCheck = true;
+                 checkPhase = ''
+                     php vendor/bin/phpunit
+                     touch $out
+                 '';
+             };
          });
-    };
+     };
   in
     jobs
